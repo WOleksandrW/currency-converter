41f85b78cdea21664b8687c314b960e9f0be3a38
WOleksandrW <oleks.221.39@gmail.com>
2024-09-19 16:34:14 +0000

    feat: add content to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useEffect } from 'react';
-import { useCurrencies } from './contexts';
-import { Header } from './components';
+import { useConverter, useCurrencies } from './contexts';
+import { CurrencyBlock, Header } from './components';
 
 import styles from './App.module.scss';
 
 function App() {
-  const { loadCurrencies } = useCurrencies();
+  const { currencies, loadCurrencies } = useCurrencies();
+  const {
+    currency1,
+    setCurrency1,
+    amount1,
+    changeAmount1,
+    currency2,
+    setCurrency2,
+    amount2,
+    changeAmount2
+  } = useConverter();
 
   useEffect(() => {
     loadCurrencies();
@@ -15,8 +25,27 @@ function App() {
     <>
       <Header />
       <main className={styles['main']}>
-        <section className={styles['section-1']}>Section 1</section>
-        <section className={styles['section-2']}>Section 2</section>
+        <section className={styles['section-1']}>
+          <h2 className={styles['title']}>Converter</h2>
+          <CurrencyBlock
+            title={currencies[currency1]}
+            currency={currency1}
+            amount={amount1}
+            onChangeCurrency={setCurrency1}
+            onChangeAmount={changeAmount1}
+          />
+          <CurrencyBlock
+            title={currencies[currency2]}
+            currency={currency2}
+            amount={amount2}
+            onChangeCurrency={setCurrency2}
+            onChangeAmount={changeAmount2}
+          />
+        </section>
+        <section className={styles['section-2']}>
+          <h2 className={styles['title']}>Graphics</h2>
+          <p className={styles['message']}>In progress...</p>
+        </section>
       </main>
     </>
   );
