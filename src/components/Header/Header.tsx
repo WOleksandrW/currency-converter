b7747e29e7bfe79eb2af37043d3160878a313002
WOleksandrW <oleks.221.39@gmail.com>
2024-09-19 14:12:51 +0000

    feat: add axios requests to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from 'react';
+import api from '../../api';
 import { CourseItem } from '../';
+import { CoursesObjType } from '../../types/CurrenciesTypes';
 
 import styles from './Header.module.scss';
 
 export default function Header() {
+  const [usdCourse, setUsdCourse] = useState<CoursesObjType>({});
+  const [eurCourse, setEurCourse] = useState<CoursesObjType>({});
+
+  useEffect(() => {
+    api.currencies
+      .getCurrencyCourse('usd')
+      .then((res) => setUsdCourse(res.data['usd']))
+      .catch((err) => console.log(err));
+    api.currencies
+      .getCurrencyCourse('eur')
+      .then((res) => setEurCourse(res.data['eur']))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <header className={styles['header']}>
       <h2 className={styles['logo']}>
         <span className={styles['accent']}>Currency</span> Converter
       </h2>
       <div className={styles['courses-list']}>
-        <CourseItem title="usd" value={45.3124512} />
-        <CourseItem title="eur" value={43.34564} />
+        <CourseItem title="usd" value={usdCourse['uah'] ?? 0} />
+        <CourseItem title="eur" value={eurCourse['uah'] ?? 0} />
       </div>
     </header>
   );
