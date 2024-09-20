23307f05d95a23b7d832063d5e655367b2d7b977
WOleksandrW <oleks.221.39@gmail.com>
2024-09-20 16:05:37 +0000

    feat: add diagram placeholder to DiagramSection component

--- src/pages/HomePage/components/DiagramSection/DiagramSection.tsx
+++ src/pages/HomePage/components/DiagramSection/DiagramSection.tsx
@@ -1,3 +1,4 @@
+import img from '../../../../assets/diagram.png';
 import styles from './DiagramSection.module.scss';
 
 export default function DiagramSection() {
@@ -5,6 +6,7 @@ export default function DiagramSection() {
     <section className={styles['diagram-section']}>
       <h2 className={styles['title']}>Diagram</h2>
       <p className={styles['message']}>In progress...</p>
+      <img className={styles['diagram']} src={img} alt="Diagram placeholder" />
     </section>
   );
 }
