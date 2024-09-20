4cb82689bbca0e6865d3d376c924e0de2a5188c0
WOleksandrW <oleks.221.39@gmail.com>
2024-09-20 12:29:01 +0000

    feat: add search to CurrencySelect component

--- src/components/CurrencySelect/CurrencySelect.tsx
+++ src/components/CurrencySelect/CurrencySelect.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useCallback } from 'react';
+import { RefObject, useCallback, useEffect, useMemo, useState } from 'react';
 import { useCurrencies } from '../../contexts';
 import useComponentVisible from '../../hooks/useComponentVisible';
 
@@ -14,6 +14,15 @@ export default function CurrencySelect({ activeKey, onChange }: CurrencySelectPr
   const { currencies } = useCurrencies();
   const { ref, isVisible, setIsVisible } = useComponentVisible(false);
 
+  const [search, setSearch] = useState('');
+
+  const displayList = useMemo(() => {
+    return Object.entries(currencies ?? {}).filter(([key, label]) => {
+      const value = search.toLowerCase();
+      return key.includes(value) || label.toLowerCase().includes(value);
+    });
+  }, [currencies, search]);
+
   const onClickHandle = useCallback(
     (value: string) => {
       onChange(value);
@@ -22,28 +31,48 @@ export default function CurrencySelect({ activeKey, onChange }: CurrencySelectPr
     [onChange, setIsVisible]
   );
 
+  useEffect(() => {
+    if (!isVisible) setSearch('');
+  }, [isVisible]);
+
   return (
     <div className={styles['select']} ref={ref as RefObject<HTMLDivElement>}>
       <div className={styles['display']} onClick={() => setIsVisible((prev) => !prev)}>
-        {activeKey ? (
-          <>
-            <span className={styles['key']}>({activeKey})</span>{' '}
-            {currencies && currencies[activeKey]}
-          </>
+        {!isVisible ? (
+          activeKey ? (
+            <p className={styles['text']}>
+              <span className={styles['key']}>({activeKey})</span>{' '}
+              {currencies && currencies[activeKey]}
+            </p>
+          ) : (
+            'Select'
+          )
         ) : (
-          'Select'
+          <input
+            className={styles['search-bar']}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onClick={(e) => e.stopPropagation()}
+            type="text"
+            placeholder="Search..."
+            autoFocus
+          />
         )}
         {!isVisible ? <FaChevronDown /> : <FaChevronUp />}
       </div>
-      <ul className={styles['list']}>
-        {isVisible &&
-          currencies &&
-          Object.entries(currencies).map(([key, label]) => (
-            <li className={styles['option']} key={key} onClick={() => onClickHandle(key)}>
-              <span className={styles['key']}>({key})</span> {label}
-            </li>
-          ))}
-      </ul>
+      {isVisible && (
+        <ul className={styles['list']}>
+          {displayList.length > 0 ? (
+            displayList.map(([key, label]) => (
+              <li className={styles['option']} key={key} onClick={() => onClickHandle(key)}>
+                <span className={styles['key']}>({key})</span> {label}
+              </li>
+            ))
+          ) : (
+            <li className={styles['empty-message']}>List is empty</li>
+          )}
+        </ul>
+      )}
     </div>
   );
 }
